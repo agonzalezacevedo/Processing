26c2f16271df1a9f386e3d8c0b7f7360ebca17ad
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-05-22 21:03:35 +0200

    Creación de la nueva rama

---
+++ src/main/java/Imagenes.java
@@ -0,0 +1,41 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Imagenes extends PApplet {
+
+    int viewport_w = 500; // width in pixels
+    int viewport_h = 500; // high in pixels
+
+    public static void main(String args[]) {
+        PApplet.main("Imagenes");
+    }
+
+    // Method that has the initial configuration
+    @Override
+    public void settings() {
+        size(viewport_w, viewport_h, P2D);
+        smooth(8);
+    }
+
+    //Objects declared for use them in all the class
+    PImage gatoPonido;
+    PImage cuchillo;
+    PImage enfadasion;
+
+    //Method that just runs one time
+    @Override
+    public void setup() {
+        // Window color un RGB
+        background(176, 250, 255);
+        //Initialized images
+        gatoPonido = loadImage(dataPath("gatoPonidoAqui.jpg"));
+        cuchillo = loadImage(dataPath("cuchillo.png"));
+        enfadasion = loadImage(dataPath("cejasGato.png"));
+    }
+
+    @Override
+    public void draw() {
+        image(gatoPonido, 0, 0, 500, 500);
+
+    }
+}
